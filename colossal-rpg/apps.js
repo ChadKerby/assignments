52b6d4c731b8b3f2ede9195ae8b1fcfca923b33c
Chad Kerby <chad.kerby24@gmail.com>
2023-03-01 10:13:03 -0700

    made changes to fight mechanic in the rpg

---
+++ colossal-rpg/apps.js
@@ -0,0 +1,181 @@
+const readline = require("readline-sync");
+let gameOver = false;
+let inCombat = true;
+let isPlayerAlive = true;
+let isEnemyAlive = true;
+let plasmaPistol = "Plasma Pistol";
+// let kills = 0
+
+// Greeting and Name
+const greeting = readline.question(
+  "\n Commander! Thank goodness you are okay, after that landing I thought you were done for.\n Now before you start exploring I need your identifier code, please enter it on your datapad. "
+);
+let name = readline.question("\nEnter you indentifier code: ");
+
+// Player Stats
+let player = {
+  name: name,
+  hp: 100,
+  inventory: [],
+};
+
+// Enemy Stats
+let enemy = {
+  hp: 100,
+};
+
+
+// Enemy List
+function enemies() {
+  let enemyList = ["Blorp", "Gamorrean", "Wookie Extremist"];
+  // Use a new var to make a random enemy appear
+  var randomEnemy = randomNum(0, 2);
+  // Assign random enemy to var
+  var newEnemy = enemyList[randomEnemy];
+  fight(newEnemy);
+}
+
+// Random Number Generator(used to pull random items from arrays)
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Enemy Drops
+function enemyDrops() {
+  var drop = ["Credits", "Blue Cheese", "Broken Blaster"];
+  // Use randomNum to pull from array above
+  var randomDrop = randomNum(0, 2);
+  // The newItem var takes the drop var and randomDrop var and ties what is generated to the newItem
+  var newItem = drop[randomDrop];
+  // Then we use .push to push the newItem into our player inventory
+  player.inventory.push(newItem);
+  return newItem;
+}
+
+
+// Damage
+function hitDamage(hit) {
+  hit = randomNum(1, 50);
+  return hit;
+}
+
+// This function reflects the damge your character takes on a hit
+function playerHealth() {
+  player.hp = player.hp - hitDamage()
+  return player.hp;
+}
+
+// Heal
+function heal() {
+  player.hp = player.hp + 20;
+  return player.hp;
+}
+
+// This function reflects the damage the enemy takes on a hit
+function enemyHealth() {
+  enemy.hp = enemy.hp - hitDamage();
+  return enemy.hp;
+}
+
+// Fight
+function fight(newEnemy) {
+    while (player.hp > 0 && enemy.hp > 0) {
+        //  isAlive();
+        var attack = readline.question( "\nYou have encountered a " + newEnemy + " Press 'a' to attack or 'r' to run. ");
+      if (attack === 'a') {
+        enemyHealth();
+        playerHealth();
+        console.log(`\nYou choose to attack with ${plasmaPistol} \n\nthe enemy has ${enemy.hp} You have ${player.hp}`);
+    } else if(attack === 'r'){
+      run();
+    }
+  } 
+    if (player.hp <= 0) {
+        console.log("\nYou have died! ");
+        inCombat = false;
+        gameOver = true;
+        console.log("GAME OVER");
+  } else if (enemy.hp <= 0){
+        inCombat = false;
+        console.log("The enemy dropped " + enemyDrops());
+  } 
+}
+
+//WALK This while loop controls the movement and encounters
+while (!gameOver) {
+  var action = readline.question("Press 'w' to walk, 'i' to check inventory or 'h' to heal: ");
+  if (action === 'i') {
+    console.log(player);
+  } else if (action === 'h') {
+    heal();
+  } else if (action === 'w' && doesEnemyAppear()) {
+    enemies();
+  }
+}
+
+// Random enemy appearance
+function doesEnemyAppear() {
+  var randomNumber = Math.floor(Math.random() * 3 + 1);
+  if (randomNumber === 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Run
+function run() {
+  runChance = Math.floor(Math.random() * 2 + 1);
+  if (runChance === 1) {
+    console.log("\nSorry, you died. Please try again ");
+    gameOver = true;
+  } else {
+    console.log("\nYour lucky...keep playing! ");
+  }
+}
+
+
+
+
+
+
+
+
+// Code that I tried
+
+// let enemy = [{
+//       name: "Blorp",
+//       hp: 100,
+//   },{
+//       name: "Gamorrean",
+//       hp: 100,
+//   },{
+//       name: "Wookie Extremist",
+//       hp: 100,
+//   }]
+
+//   function enemies(enemy) {
+//     for(var i = 0; i < enemy.lenght; i++)
+//     // let enemyList = ["Blorp", "Gamorrean", "Wookie Extremist"];
+//     // Use a new var to make a random enemy appear
+//     var randomEnemy = randomNum(0, 2);
+//     // Assign random enemy to var
+//     var newEnemy = [i][randomEnemy];
+//     fight(newEnemy);
+//   }
+
+  
+ // // function isAlive(){
+//   if (player.hp <= 0) {
+//     console.log("\nYou have died! ");
+//     inCombat = false;
+//     gameOver = true;
+//     console.log("GAME OVER");
+//   } else if (enemy.hp <= 0){
+//       inCombat = false;
+//       console.log("The enemy dropped " + enemyDrops());
+//   } 
+//   console.log(`\nYou choose to attack with ${newPistol} \n\nthe enemy has ${enemy.hp} You have ${player.hp}`)
+// }
+  
+ 
--- problem-solving-practice/apps.js
+++ problem-solving-practice/apps.js
@@ -1,25 +1,28 @@
 // Write a function that takes an array of numbers and returns the largest (without using Math.max())
-function largest(array) {
-    // array[0] starts the function at the 0 index
-    var value = array[0];
+// function largest(array) {
+//     // array[0] starts the function at the 0 index
+//     var value = array[0];
 
-    for (var i = 1; i < array.length; i++) {
-        if (array[i] > value) value = array[i];
-    }
-    return value;
-}
-console.log(largest([6, 13, 250, 3]));
-console.log(largest([3, 5, 2, 8, 1]));
-console.log(largest([-13, 40, 3, 0, 19, 22]));
+//     for (var i = 1; i < array.length; i++) {
+//         if (array[i] > value) value = array[i];
+//     }
+//     return value;
+// }
+// console.log(largest([6, 13, 250, 3]));
+// console.log(largest([3, 5, 2, 8, 1]));
+// console.log(largest([-13, 40, 3, 0, 19, 22]));
 
 // Write a function that takes an array of words and a character and returns each word that has that character present.
-
-
-
-
-
-
-
+function lettersWithStrings(arr, char) {
+    let stringsWithLetters = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].includes(char)) {
+        stringsWithLetters.push(arr[i]);
+      }
+    }
+    return stringsWithLetters;
+  }
+  
 // test data
 console.log(lettersWithStrings(["$hello!", "%%^%%", "test!"], "!")) // => ["$hello!", "test!"]
 console.log(lettersWithStrings(["#3", "$$$", "C%4!", "Hey!"], "!"))  // => ["C%4!", "Hey!"]
@@ -31,6 +34,6 @@ console.log(lettersWithStrings(["yellow", "green", "^up^", "down", "dog"], "h"))
 
 
 // test data
-console.log(isDivisible(4, 2)) // => true
-console.log(isDivisible(9, 3)) // => true
-console.log(isDivisible(15, 4)) // => false
+// console.log(isDivisible(4, 2)) // => true
+// console.log(isDivisible(9, 3)) // => true
+// console.log(isDivisible(15, 4)) // => false
